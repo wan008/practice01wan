187468991bf5485669331cd7a64585d3aef8f3a1
wan008 <kimw@sheridancollege.ca>
2023-05-29 11:39:16 -0400

    Added print statement for this is my life.

--- src/practicewantae01/PracticeWanTae01.java
+++ src/practicewantae01/PracticeWanTae01.java
@@ -8,6 +8,9 @@ public class PracticeWanTae01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Start source code management");
+        
+        //add local in Netbean for testing
+        System.out.println("This is my life");
     }
     
 }
