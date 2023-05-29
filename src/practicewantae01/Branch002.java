efd85a71b598f017ed3824e3efd3524a13bd11ec
wan008 <kimw@sheridancollege.ca>
2023-05-29 15:30:20 -0400

    Add Branch002.java for new branch.

---
+++ src/practicewantae01/Branch002.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicewantae01;
+
+
+public class Branch002 {
+    private int branch02;
+
+    public Branch002(int branch02) {
+        this.branch02 = branch02;
+    }
+
+    public int getBranch02() {
+        return branch02;
+    }
+
+    public void setBranch02(int branch02) {
+        this.branch02 = branch02;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch002{" + "branch02=" + branch02 + '}';
+    }
+    
+    
+}
