752e274f4b9be7a025086d6029c15c5f605f8f56
wan008 <kimw@sheridancollege.ca>
2023-05-29 10:21:14 -0400

    Added print statement for testing.

--- src/practicewantae01/PracticeWanTae01.java
+++ src/practicewantae01/PracticeWanTae01.java
@@ -8,6 +8,8 @@ public class PracticeWanTae01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Start source code management");
+        // first added lines
+        System.out.println("This is good for working.");
     }
     
 }
